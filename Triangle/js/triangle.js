f85384559e2721de76ba4ae6b2e44c45d22d251d
monialisa91 <monikarichter18@gmail.com>
2017-08-25 15:48:06 +0200

    Area of a triangle

---
+++ Triangle/js/triangle.js
@@ -0,0 +1,11 @@
+
+//Declaration of the parameters
+
+var h= prompt("Enter the height of a triangle");
+var a= prompt("Enter the base of the triangle");
+
+//Calculation of the triangle's area
+
+var triangleArea=h*a/2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
